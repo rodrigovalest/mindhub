a96d389399bd3dea36b096fb5b59b64792077200
Rodrigo <rodrigo.vale.st@gmail.com>
2023-10-01 18:19:00 -0300

    [del + add] removing old project + creating new ts project structure

--- frontend/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar({ children }) {
-  return (
-    <nav className="flex justify-between items-center py-2 px-2">
-      {children}
-    </nav>
-  );
-}
--- frontend/src/pages/auth/Signout.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function Signout() {
-  localStorage.removeItem("token");
-
-  return (
-    <Navigate to="/auth/signin" />
-  );
-}
